2e5959e22cd6c00de91111095ae04082ae28ee16
ngallion <tndg1@unigroupinc.com>
2018-12-01 19:48:44 -0600

    removed favicon

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <h1>Driveline Assessment Generator</h1>
         </header>
         <body>
           <div id="main">
